c40867c596698ab801a70ca6130c20d88147417d
Mick Kirkegaard <mk@circleconsult.dk>
2020-10-31 12:38:44 +0100

    Baseline

--- __init__.py
+++ __init__.py
@@ -7,167 +7,56 @@ __license__ = 'GPL v3'
 __copyright__ = '2020, Mick Kirkegaard (mickkn@gmail.com)'
 __docformat__ = 'restructuredtext el'
 
-import time
-from urllib import quote
 import socket
-import re
-import json
 import datetime
 from threading import Thread
-from calibre import as_unicode
-from calibre import browser
 from calibre.ebooks.metadata.sources.base import Source
-from calibre.utils.cleantext import clean_ascii_chars
 from calibre.ebooks.metadata.book.base import Metadata
-from calibre.library.comments import sanitize_comments_html
-from calibre.utils.cleantext import clean_ascii_chars
-from Queue import Queue, Empty
 
 
 class Saxo(Source):
     name = 'Saxo'
-    description = _('Downloads Metadata and covers from saxo.dk')
+    description = _('Downloads Metadata and covers from Saxo.dk')
     supported_platforms = ['windows', 'osx', 'linux']
     author = 'Mick Kirkegaard'
-    version = (0, 0, 1)
+    version = (1, 0, 0)
     minimum_calibre_version = (0, 8, 4)
 
     capabilities = frozenset(['identify', 'cover'])
-    # , 'cover'
-    touched_fields = frozenset(
-        ['identifier:isbn', 'identifier:biblionetid', 'title', 'authors', 'tags', 'publisher', 'pubdate', 'series'])
-    # 'identifier:isbn',
-    # 'publisher',
-    # 'pubdate',
-    # 'tags',
-    # 'series'
+    touched_fields = frozenset(['identifier:isbn', 'title', 'authors', 'tags', 'publisher', 'pubdate', 'series'])
 
     supports_gzip_transfer_encoding = True
 
-    # http://bookmeta.phpfogapp.com is currently offline
-    # Please follow instructions created by nikan on https://github.com/nikan/bookmeta
-    # and change the ‘http://bookmeta.phpfogapp.com/‘ to the address of your server
-    BASE_URL = 'https://www.saxo.com/dk/products/search?query='
+    BASE_URL = 'http://metablogging.gr/bookmeta/index.php?isbn='
 
     def get_book_url(self, identifiers):
-        isbn = identifiers.get('isbn', None)
-        if isbn:
-            return 'biblionet', isbn, '%s%s' % (Saxo.BASE_URL, isbn)
+        return
 
-    def identify(self, log, result_queue, abort, title=None, authors=None, identifiers=None, timeout=30):
-        """
-        Note this method will retry without identifiers automatically if no
-        match is found with identifiers.
-        """
-        print("identify")
-        print("    Identifiers are: ", identifiers)
-
-        matches = []
-
-        br = self.browser
-        isbn = identifiers.get('isbn', None)
-        if isbn:
-            print("    Found isbn %s" % isbn)
-            matches.append('%s%s' % (Saxo.BASE_URL, isbn))
-        if abort.is_set():
-            return
-
-        print("    Matches are: ", matches)
-        log.info("    Matches are: ", matches)
-
-        # from calibre_plugins.biblionet.worker import Worker
-        workers = [Worker(url, result_queue, br, log, i, self) for i, url in enumerate(matches)]
-
-        for w in workers:
-            w.start()
-            # Don't send all requests at the same time
-            time.sleep(0.1)
-
-        while not abort.is_set():
-            a_worker_is_alive = False
-            for w in workers:
-                w.join(0.2)
-                if abort.is_set():
-                    break
-                if w.is_alive():
-                    a_worker_is_alive = True
-            if not a_worker_is_alive:
-                break
-
-        return None
+    def identify(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
+        return
 
     def get_cached_cover_url(self, identifiers):
-        url = None
-        biblionet = identifiers.get('biblionet', None)
-        if biblionet is None:
-            isbn = identifiers.get('isbn', None)
-            if isbn is not None:
-                biblionet = self.cached_isbn_to_identifier(isbn)
-        if biblionet is not None:
-            url = self.cached_identifier_to_cover_url(biblionet)
-        return url
+        return
 
     def cached_identifier_to_cover_url(self, id_):
-        with self.cache_lock:
-            url = self._get_cached_identifier_to_cover_url(id_)
-            if not url:
-                # Try for a "small" image in the cache
-                url = self._get_cached_identifier_to_cover_url('small/' + id_)
-            return url
+        return
 
     def _get_cached_identifier_to_cover_url(self, id_):
-        # This must only be called once we have the cache lock
-        url = self._identifier_to_cover_url_cache.get(id_, None)
-        if not url:
-            key_prefix = id_.rpartition('/')[0]
-            for key in self._identifier_to_cover_url_cache.keys():
-                if key.startswith('key_prefix'):
-                    return self._identifier_to_cover_url_cache[key]
-        return url
+        return
 
     def download_cover(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
-        cached_url = self.get_cached_cover_url(identifiers)
-        if cached_url is None:
-            log.info('No cached cover found, running identify')
-            rq = Queue()
-            self.identify(log, rq, abort, title=title, authors=authors, identifiers=identifiers)
-            if abort.is_set():
-                return
-            results = []
-            while True:
-                try:
-                    results.append(rq.get_nowait())
-                except Empty:
-                    break
-            results.sort(key=self.identify_results_keygen(
-                title=title, authors=authors, identifiers=identifiers))
-            for mi in results:
-                cached_url = self.get_cached_cover_url(mi.identifiers)
-                if cached_url is not None:
-                    break
-        if cached_url is None:
-            log.info('No cover found')
-            return
-
-        if abort.is_set():
-            return
-        br = self.browser
-        log('Downloading cover from:', cached_url)
-        try:
-            cdata = br.open_novisit(cached_url, timeout=timeout).read()
-            result_queue.put((self, cdata))
-        except:
-            log.exception('Failed to download cover from:', cached_url)
+        return
 
 
 class Worker(Thread):  # Get details
 
-    """
-    Get book details from Biblionet book page in a separate thread
-    """
+    '''
+    Get book details from Saxos book page in a separate thread
+    '''
 
     def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=20):
         Thread.__init__(self)
+        self.title = None
         self.daemon = True
         self.url = url
         self.result_queue = result_queue
@@ -177,7 +66,6 @@ class Worker(Thread):  # Get details
         self.plugin = plugin
         self.browser = browser.clone_browser()
         self.cover_url = None
-        self.biblionetid = None
         self.series_index = None
         self.authors = []
         self.yr_msg1 = 'No publishing year found'
@@ -195,117 +83,133 @@ class Worker(Thread):  # Get details
         self.log.info("        self:     ", self)
         self.log.info("        self.url: ", self.url)
 
+        # Get some data from the website
         try:
-            raw = self.browser.open_novisit(self.url, timeout=self.timeout).read().strip()
-            self.log.info(raw)
-        except Exception as e:
-            if callable(getattr(e, 'getcode', None)) and e.getcode() == 404:
-                self.log.error('URL malformed: %r' % self.url)
-                return
-            attr = getattr(e, 'args', [None])
-            attr = attr if attr else [None]
-            if isinstance(attr[0], socket.timeout):
-                msg = 'Bookmeta for biblionet timed out. Try again later.'
-                self.log.error(msg)
-            else:
-                msg = 'Failed to make details query: %r' % self.url
-                self.log.exception(msg)
-            return
-
-        if '<title>404 - ' in raw:
-            self.log.error('URL malformed: %r' % self.url)
-            return
-
-        try:
-            # root = fromstring(clean_ascii_chars(raw))
-            root = json.loads(raw)
-            self.log.info(root)
+            self.log.info("Fetch some data from the website here")
         except:
-            msg = 'Failed to parse book detail page: %r' % self.url
-            self.log.exception(msg)
             return
 
-        try:
-            self.biblionetid = root['biblionetid']
-        except:
-            self.log.exception('Error parsing book id for url: %r' % self.url)
-            self.biblionetid = None
+        # Do some error handling
+        #if '<title>404 - ' in raw:
+        #    self.log.error('URL malformed: %r' % self.url)
+        #    return
 
+        # Clean the html data
         try:
-            self.title = root['title'].strip()
+            self.log.info("Clean the html data here")
+        except:
+            return
+
+        # Strip the title of the book
+        try:
+            #self.title = root['title'].strip()
+            self.title = "Test title"
+            self.log.info("Strip the book title here")
         except:
             self.log.exception('Error parsing title for url: %r' % self.url)
-            self.title = None
-            self.series_index = None
 
+        # Strip the aithor of the book here
         try:
-            self.authors = [root['authors'].strip()]
-            self.log.info(self.authors)
+            #self.authors = [root['authors'].strip()]
+            #self.log.info(self.authors)
+            self.authors = "Test Author"
+            self.log.info("Strip the book authors here")
         except:
             self.log.exception('Error parsing authors for url: %r' % self.url)
             self.authors = None
 
+        # Strip the URL for cover here
         try:
-            self.cover_url = root['cover_url']
-            self.log.info('Parsed URL for cover:%r' % self.cover_url)
-            self.plugin.cache_identifier_to_cover_url(self.biblionetid, self.cover_url)
+            #self.cover_url = root['cover_url']
+            #self.log.info('Parsed URL for cover:%r' % self.cover_url)
+            #self.plugin.cache_identifier_to_cover_url(self.biblionetid, self.cover_url)
+            self.cover_url = "Test cover url"
+            self.log.info("Strip cover url here")
         except:
             self.log.exception('Error parsing cover for url: %r' % self.url)
             self.has_cover = bool(self.cover_url)
 
+        # Strip the book Publisher here
         try:
-            self.publisher = root['publisher']
-            self.log.info('Parsed publisher:%s' % self.publisher)
+            #self.publisher = root['publisher']
+            #self.log.info('Parsed publisher:%s' % self.publisher)
+            self.publisher = "Test publisher"
+            self.log.info("Strip the publisher name here")
         except:
             self.log.exception('Error parsing publisher for url: %r' % self.url)
 
+        # Strip the book tags here
         try:
-            self.tags = root['categories'].replace('DDC: ', 'DDC:').replace('-', '').split()[:-1]
-            self.log.info('Parsed tags:%s' % self.tags)
+            #self.tags = root['categories'].replace('DDC: ', 'DDC:').replace('-', '').split()[:-1]
+            #self.log.info('Parsed tags:%s' % self.tags)
+            self.tags = "Test tag"
+            self.log.info("Strip the book tags here")
         except:
             self.log.exception('Error parsing tags for url: %r' % self.url)
 
+        # Strip the year of publish here
         try:
-            self.pubdate = root['yr_published']
-            self.log.info('Parsed publication date:%s' % self.pubdate)
+            #self.pubdate = root['yr_published']
+            #self.log.info('Parsed publication date:%s' % self.pubdate)
+            self.pubdate = "Test publisher data"
+            self.log.info("Strip the year of publish here")
         except:
             self.log.exception('Error parsing published date for url: %r' % self.url)
 
-        mi = Metadata(self.title, self.authors)
-        mi.set_identifier('biblionet', self.biblionetid)
+        # Setup the metadata
+        meta_data = Metadata(self.title, self.authors)
+        meta_data.set_identifier('isbn', self.isbn)
 
         if self.series_index:
             try:
-                mi.series_index = float(self.series_index)
+                meta_data.series_index = float(self.series_index)
             except:
                 self.log.exception('Error loading series')
         if self.relevance:
             try:
-                mi.source_relevance = self.relevance
+                meta_data.source_relevance = self.relevance
             except:
                 self.log.exception('Error loading relevance')
         if self.cover_url:
             try:
-                mi.cover_url = self.cover_url
+                meta_data.cover_url = self.cover_url
             except:
                 self.log.exception('Error loading cover_url')
         if self.publisher:
             try:
-                mi.publisher = self.publisher
+                meta_data.publisher = self.publisher
             except:
                 self.log.exception('Error loading publisher')
         if self.tags:
             try:
-                mi.tags = self.tags
+                meta_data.tags = self.tags
             except:
                 self.log.exception('Error loading tags')
         if self.pubdate:
             try:
                 if self.pubdate not in (self.yr_msg1, self.yr_msg2):
                     d = datetime.date(int(self.pubdate), 1, 1)
-                    mi.pubdate = d
+                    meta_data.pubdate = d
             except:
                 self.log.exception('Error loading pubdate')
 
-        self.plugin.clean_downloaded_metadata(mi)
-        self.result_queue.put(mi)
+        self.plugin.clean_downloaded_metadata(meta_data)
+        self.result_queue.put(meta_data)
+
+
+if __name__ == '__main__':
+    # To run these test use:
+    # calibre-debug -e __init__.py
+    from calibre.ebooks.metadata.sources.test import (test_identify_plugin, title_test, authors_test, series_test)
+
+    test_identify_plugin(Saxo.name,
+                         [
+                             (  # A book with an ISBN
+                                 {'identifiers': {'isbn': '9780385340588'},
+                                  'title': '61 Hours', 'authors': ['Lee Child']},
+                                 [title_test('61 Hours', exact=True),
+                                  authors_test(['Lee Child']),
+                                  series_test('Jack Reacher', 14.0)]
+                             ),
+
+                         ])
